b1bc6b050d51615246bc7c2bdf87f21da6c07225
Jacob Weaver <jweaver60@gmail.com>
2014-02-22 22:47:16 -0500

    more changes

--- wp-content/themes/dilletantish/header.php
+++ wp-content/themes/dilletantish/header.php
@@ -15,7 +15,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href='http://fonts.googleapis.com/css?family=Grand+Hotel|Englebert|Homemade+Apple|Amatic+SC|Londrina+Sketch' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Unica+One|Dawning+of+a+New+Day' rel='stylesheet' type='text/css'>
 
 <?php wp_head(); ?>
 </head>
