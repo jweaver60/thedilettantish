016cce4bb10b833e2c4580422cac58a00d6831a9
Jacob Weaver <jweaver60@gmail.com>
2014-03-02 17:45:37 -0500

    layout changes, social icons

--- wp-content/themes/dilletantish/header.php
+++ wp-content/themes/dilletantish/header.php
@@ -15,7 +15,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<link href='http://fonts.googleapis.com/css?family=News+Cycle|Codystar' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=News+Cycle' rel='stylesheet' type='text/css'>
 <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 
 
@@ -23,28 +23,35 @@
 </head>
 
 <body <?php body_class(); ?>>
+  <!-- start container -->
+  <div class="container">
 
-	<!-- navigation -->
-	<nav class="top-bar" data-topbar>
-		<ul class="title-area">
-			<li class="name"></li>
-	    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
-	  </ul>
-	  <section class="top-bar-section">
-	    <ul class="right">
-	      <?php wp_nav_menu( array( 'container' => '', 'items_wrap' => '%3$s', 'theme_location' => 'primary' ) ); ?>
-	    </ul>
-	  </section>
-	</nav>
+    <!-- header -->
+    <div class="row">
+      <div class="small-12 medium-8 columns">
+        <header>
+          <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        </header>
+      </div>
+      <div class="small-12 medium-4 columns">
+        <ul class="social">
+          <li><a href="http://www.pinterest.com/beegeeweave/" target="_blank"><i class="fa fa-pinterest fa-2x"></i></a></li>
+          <li><a href="http://www.twitter.com/beegeebeelane/" target="_blank"><i class="fa fa-twitter fa-2x"></i></a></li>
+          <li><a href="http://www.facebook.com/bethany.lane.52" target="_blank"><i class="fa fa-facebook fa-2x"></i></a></li>
+          <li><a href="http://www.instagram.com/beebeeweave" target="_blank"><i class="fa fa-instagram fa-2x"></i></a></li>
+        </ul>
+      </div>
+    </div>
 
-	<!-- start container -->
-	<div class="container">
-
-		<!-- header -->
-		<div class="row">
-			<div class="small-12 columns">
-				<header>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				</header>
-			</div>
-		</div>
+  	<!-- navigation -->
+  	<nav class="top-bar" data-topbar>
+  		<ul class="title-area">
+  			<li class="name"></li>
+  	    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+  	  </ul>
+  	  <section class="top-bar-section">
+  	    <ul class="left">
+  	      <?php wp_nav_menu( array( 'container' => '', 'items_wrap' => '%3$s', 'theme_location' => 'primary' ) ); ?>
+  	    </ul>
+  	  </section>
+  	</nav>
--- wp-content/themes/dilletantish/sidebar.php
+++ wp-content/themes/dilletantish/sidebar.php
@@ -6,6 +6,19 @@
  */
 ?>
 	<div id="secondary" class="widget-area" role="complementary">
+    <div class="row">
+      <div class="small-12 columns">
+        <h3 class="sidebar-header">Welcome to The Dilettantish!</h3>
+      </div>
+    </div>
+    <div class="row">
+      <div class="small-12 large-6 columns">
+        <img src="<?php the_field('about_me_image', 2); ?>" alt="About Me">
+      </div>
+      <div class="small-12 large-6 columns">
+        <?php the_field('about_me_text', 2); ?>
+      </div>
+    </div>
 		<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 			<aside id="search" class="widget widget_search">
